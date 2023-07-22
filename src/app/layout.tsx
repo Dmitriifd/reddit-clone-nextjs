77ea74ba55101421a9716d66365af6c38fde6c66
Dmitriifd <idimka9@gmail.com>
2023-07-22 20:23:34 +0500

    create subreddit page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from '@/lib/utils';
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/Navbar';
 import { Toaster } from '@/components/ui/Toaster';
+import Providers from '@/components/Providers';
 
 import '@/styles/globals.css';
 
@@ -28,14 +29,16 @@ export default function RootLayout({
       )}
     >
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
-        {/* @ts-expect-error server component */}
-        <Navbar />
-        {authModal}
+        <Providers>
+          {/* @ts-expect-error server component */}
+          <Navbar />
+          {authModal}
 
-        <div className='container max-w-7xl mx-auto h-full pt-12'>
-          {children}
-        </div>
-        <Toaster />
+          <div className='container max-w-7xl mx-auto h-full pt-12'>
+            {children}
+          </div>
+          <Toaster />
+        </Providers>
       </body>
     </html>
   );
